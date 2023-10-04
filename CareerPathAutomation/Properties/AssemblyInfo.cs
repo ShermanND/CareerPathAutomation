bdeec77307120b3f289a4105ab652002bb87614a
CX761XP <CX761XP@AR3347568W1>
2023-10-04 15:15:36 -0300

    Add project files.

---
+++ CareerPathAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CareerPathAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EY")]
+[assembly: AssemblyProduct("CareerPathAutomation")]
+[assembly: AssemblyCopyright("Copyright © EY 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e4a89d5-1f73-4bc3-ac43-c890d855f4f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
