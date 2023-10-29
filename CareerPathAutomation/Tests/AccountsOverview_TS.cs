4a6ca6621fa1bd1eed8caad3755799ed20accb62
Emiliano Francavilla <Emiliano.Francavilla@gds.ey.com>
2023-10-29 13:26:51 -0300

    Emi push 2

--- CareerPathAutomation/Tests/AccountsOverview_TS.cs
+++ CareerPathAutomation/Tests/AccountsOverview_TS.cs
@@ -11,6 +11,7 @@ namespace CareerPathAutomation
     {
         IWebDriver driver;
         AccountsOverview_POM accountsoverviewpom;
+        //test commit
 
         [OneTimeSetUp]
         public void Setup()
