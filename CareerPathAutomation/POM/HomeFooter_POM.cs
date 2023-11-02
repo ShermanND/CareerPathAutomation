f9735b0e9fda5dbbc3d0b2b5435ffbf4e5992daf
German Nostel Dominguez <ruloaas@hotmail.com>
2023-11-02 16:08:04 -0300

    Cambios 02 11 23

---
+++ CareerPathAutomation/POM/HomeFooter_POM.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HomeFooter_POM
+{
+    private IWebDriver driver;
+
+    public HomeFooter_POM(IWebDriver driver)
+	{
+        this.driver = driver;
+    }
+
+    // SELECTORS
+    public string homeFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[1]/a";
+    public string aboutUsFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[2]/a";
+    public string servicesFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[3]/a";
+    public string productsFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[4]/a";
+    public string locationsFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[5]/a";
+    public string forumFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[6]/a";
+    public string siteMapFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[7]/a";
+    public string contactUsFooter = "//*[@id=\"footerPanel\"]/ul[1]/li[8]/a";
+
+    // HTML TITLE
+    public string title = "ParaBank | Welcome | Online Banking";
+
+    // METHODS
+    public void NavigateToHomeFooter()
+    {
+        driver.findElement(By.xpath(homeFooter)).click();
+    }
+
+    public void NavigateToAboutUsFooter()
+    {
+        driver.findElement(By.xpath(aboutUsFooter)).click();
+    }
+
+    public void NavigateToServicesFooter()
+    {
+        driver.findElement(By.xpath(servicesFooter)).click();
+    }
+
+    public void NavigateToProductsFooter()
+    {
+        driver.findElement(By.xpath(productsFooter)).click();
+    }
+
+    public void NavigateToLocationsFooter()
+    {
+        driver.findElement(By.xpath(locationsFooter)).click();
+    }
+
+    public void NavigateToForumFooter()
+    {
+        driver.findElement(By.xpath(forumFooter)).click();
+    }
+
+    public void NavigateToSiteMapFooter()
+    {
+        driver.findElement(By.xpath(siteMapFooter)).click();
+    }
+
+    public void NavigateToContactUsFooter()
+    {
+        driver.findElement(By.xpath(contactUsFooter)).click();
+    }
+
+}
---
+++ CareerPathAutomation/Tests/HomeFooter_TS.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace CareerPathAutomation
+{
+    [TestFixture]
+    public class HomeFooter
+	{
+        IWebDriver driver;
+        HomeFooter_POM homefooterpom;
+
+        static void Main() { }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Driver_POM driverpom = new Driver_POM(driver);
+            driverpom.DriverSetUp();
+            homefooterpom = new HomeFooter_POM(driver);
+            Thread.Sleep(3000);
+        }
+
+        [Test, Order(1)]
+        [Category("Test Case 71: Accessing Home Footer")]
+        public void Test0101()
+        {
+            // Given I have accessed to Parabank website
+            // When I navigate to Home screen
+            // Then I verify that Home in the Footer is working
+            homefooterpom.NavigateToHomeFooter();
+            string title = driver.Title;
+            Assert.AreEqual(homefooterpom.title, title);
+        }
+
+
+    }
+}
